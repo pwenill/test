1bf434271cc8cd5bbc7b932e7c387af5f09c9cc1
Pwenill <Calpetard27@gmail.com>
2024-07-11 20:35:31 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 4000 | process.env.PORT;
+const port = process.env.PORT | 4000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
