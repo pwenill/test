812f56d7c43d136db74256aa619132f0fd3080d2
Pwenill <Calpetard27@gmail.com>
2024-07-12 11:41:45 +0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
 });
